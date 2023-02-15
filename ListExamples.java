109b015d5ccdc59bfec829cc6463c4ddf8346066
lpaulette <lgutierrezramirez@ucsd.edu>
2023-02-15 12:48:43 -0800

    Update ListExamples.java

--- ListExamples.java
+++ ListExamples.java
@@ -32,7 +32,7 @@ public class ListExamples  implements StringChecker {
   // Takes two sorted list of strings (so "a" appears before "b" and so on),
   // and return a new list that has all the strings in both list in sorted order.
   static List<String> merge(List<String> list1, List<String> list2) {
-    List<String> result = new ArrayList<>();
+    /*List<String> result = new ArrayList<>();
     int index1 = 0, index2 = 0;
     while(index1 < list1.size() && index2 < list2.size()) {
       if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
@@ -51,7 +51,7 @@ public class ListExamples  implements StringChecker {
     while(index2 < list2.size()) {
       result.add(list2.get(index2));
       index1 += 1;//add to index2 not index1
-    }
+    }*/
 
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     return expected;
